ebb7b6a43e2cd82b9fd6a9068037c97720123796
akaevjumgal <akaev.jumgal@gmail.com>
2021-06-07 18:21:01 +0600

    updated ramda file

--- utils/ramda.ts
+++ utils/ramda.ts
@@ -1,2 +1,7 @@
-export const pipe = (...fns) => x => fns.reduce((v, f) => f(v), x)
-export const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x)
+const pipe = <T>(...fns: Array<(arg: T) => T>) => (x: T) => fns.reduce((v, f) => f(v), x)
+const compose = <T>(...fns: Array<(arg: T) => T>) => (x: T) => fns.reduceRight((v, f) => f(v), x)
+
+export {
+	pipe,
+	compose,
+}
